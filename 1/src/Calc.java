6e020875abeaa808e794ccb95dc055ca9f8682f5
ASUS <7014649@gmail.com>
2015-11-01 15:17:01 +0200

    Simple calculator

--- 1/src/Calc.java
+++ 1/src/Calc.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 
 public class Calc {
-    public static void main (String []args) {
+    public static void main (String [] args) {
         Scanner scanner = new Scanner(System.in);
         int n1, n2, sum, difference, multiplication, division;
 
