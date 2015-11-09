482090e87312a8b641de5f80ea4a9592a0df39e4
ASUS <7014649@gmail.com>
2015-11-09 11:55:01 +0200

    Simple calculator

--- 1/src/Calc.java
+++ 1/src/Calc.java
@@ -1,35 +1,95 @@
 /**
  * Created by ASUS on 31.10.2015.
  */
+
 import java.util.Scanner;
 
 
+//public class Calc {
+//  public static void main(String[] args) {
+//Scanner scanner = new Scanner(System.in);
+//double n1, n2, sum, difference, multiplication, division;
+
+//System.out.println("Enter first number: ");
+//n1 = scanner.nextDouble();
+//System.out.println("Enter second number: ");
+//n2 = scanner.nextDouble();
+//sum = n1 + n2;
+//difference = n1 - n2;
+//multiplication = n1 * n2;
+//System.out.println("Sum:" + sum);
+//System.out.println("Difference:" + difference);
+//System.out.println("Multiplication:" + multiplication);
+//if (n2 == 0) {
+//  System.out.println("Incorrect-Division to zero");
+// } else {
+//   division = n1 / n2;
+// System.out.println("Division:" + division);
+// }
+
+
+//????? ??????????? ??? ?????
+//public class Calc {
+  //  public static void main(String[] args) {
+        // double val = Calc(2.3, 23);
+        //System.out.println(val);
+        //}
+//    public static double Calc(double first, double second) {
+        //      double sum = first + second;
+        //     return sum;
+
+
+        // ????? ?????????? ????? ???? ? ?????
+
+//        int val = Calc(202);
+        //      System.out.println(val);
+        //    }
+
+        //  public static int Calc(int first) {
+        //    int i;
+        //  for (i = 0; i > 0; i++) ;
+        //int x = first % 10;
+        //int sum = x + x;
+        // return sum;
+        //}
+//}
+
+        //????? ?????????? ????? ? ??????????????
+
+
+//        int xp, yp, xa, xb, ya, yb;
+        //      Scanner scanner = new Scanner(System.in);
+        //    System.out.println("Enter xa :");
+        //  xa = scanner.nextInt();
+//        System.out.println("Enter ya :");
+        //      ya = scanner.nextInt();
+        //    System.out.println("Enter xb :");
+        //  xb = scanner.nextInt();
+//        System.out.println("Enter yb :");
+        //      yb = scanner.nextInt();
+        //    System.out.println("Enter xp :");
+        //  xp = scanner.nextInt();
+        //System.out.println("Enter yp :");
+        //yp = scanner.nextInt();
+
+        //if ((xp > xa) && (xp < xb) && (yp < ya) && (yp > yb))
+        // System.out.println("ok");
+        //else {
+        //  System.out.println("no");
+        // }
+        // }
+//}
+
+//define i-Fibonachi
 public class Calc {
-    public static void main (String [] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n1, n2, sum, difference, multiplication, division;
-
-            System.out.println("Enter first number: ");
-            n1 = scanner.nextInt();
-            System.out.println("Enter second number: ");
-            n2 = scanner.nextInt();
-            sum = n1 + n2;
-            difference = n1 - n2;
-            multiplication = n1 * n2;
-        System.out.println("Sum:" + sum);
-        System.out.println("Difference:" + difference);
-        System.out.println("Multiplication:" + multiplication);
-
-         try {
-             division = n1 / n2;
-             System.out.println("Division:" + division);
-         }
-        catch (ArithmeticException e){
-            System.out.println("Incorrect-Division to zero");
-        }
-               }
-
+    public static void main(String[] args) {
+        int choise = fibonachi(15);
+        System.out.println("Fibonachi number is :" +choise);
     }
-
-
+        static int fibonachi ( int i){
+            if ((i == 0) || (i==1))
+                return 1;
+                return fibonachi(i - 1) + fibonachi(i - 2);
+            }
+        }
 
